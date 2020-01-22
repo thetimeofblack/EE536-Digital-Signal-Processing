33647c350cbe0d91fa30cd5073b33fafd4c78095
thetimeofblack <1191401005@qq.com>
2020-01-22 13:05:07 -0800

    too much

---
+++ hw1/bm3d/BM3D_Denoising.m
@@ -0,0 +1,10 @@
+G = readraw('../HW1_images/Corn_noisy.raw'); 
+Y = readraw('../HW1_images/Corn_gray.raw')
+x = im2double(G); 
+[NA, y_est] = BM3D(Y, x, sigma); 
+final_result = y_est*255; 
+writeraw(final_result,'Corn_bm3d.raw');
+
+
+
+
